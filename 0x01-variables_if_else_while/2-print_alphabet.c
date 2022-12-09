ffd835e3d9f94053aa917ba8dfafe297183f6004
Clémentine AMEVOR <clementineamevor97@gmail.com>
2022-12-09 15:45:47 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+# include <stdio.h>
+/**
+ * main - My learning programm c
+ *
+ * Return : 0
+ */
+int main(void)
+{
+	int i, j;
+
+	i = 9;
+	j = 65;
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+	while (j < 91)
+	{
+		putchar(j);
+		j++
+	}
+	putchar(10);
+	return (0);
+}

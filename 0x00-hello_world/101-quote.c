c9b04e76a5b96dd2ccf9511082ccc4b0299dd81c
Clémentine AMEVOR <clementineamevor97@gmail.com>
2022-12-08 21:59:23 -0800

    task7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - print out sizes
- * code by clementine
- * Return: 0
- */
+ * main - prints exactly and that piece of art is useful
+ * Return: Always 1(Success)
+ * */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-	return (0);
+	printf(2, "and that piece of art is useful\" - "Dora Korpar, 2015-10-19\n", 59);
+	return(1);
 }

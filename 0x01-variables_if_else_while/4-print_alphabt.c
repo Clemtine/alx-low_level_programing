1465eaecc50f02333d517fc97ec5179053dba346
Clémentine AMEVOR <clementineamevor97@gmail.com>
2022-12-09 17:24:18 -0800

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+/**
+ * main -my learning program c
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 97;
+	while (i < 123 && i != e && i != q)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+	return (0);
+}

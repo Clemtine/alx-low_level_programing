9ca12282e4a70ec1bef799e4681a83bbe47fa48b
Clémentine AMEVOR <clementineamevor97@gmail.com>
2022-12-10 15:53:54 -0800

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+/**
+ * main - my continuous learning program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c > 'a'; c--)
+		putchar(c);
+	putchar('\n);
+	return (0);
+}

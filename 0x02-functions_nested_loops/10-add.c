420ed6dd4c82805c50c1aedf646750861e0c5711
Clémentine AMEVOR <clementineamevor97@gmail.com>
2022-12-13 16:40:33 -0800

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - learning in continuous
+ * @i: first addittion letter
+ * @j: second addition letter
+ * Return: 1
+ */
+
+int add(int i, int j)
+{
+	int n;
+
+	n = i + j;
+	return (n);
+}

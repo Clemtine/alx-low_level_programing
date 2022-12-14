f78c81c1a0a7dc6e410a53d0c8c08ceebc6690a5
Clémentine AMEVOR <clementineamevor97@gmail.com>
2022-12-13 17:09:06 -0800

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,14 @@
 #include "main.h"
 /**
  * print_last_digit - learning
- *
+ * @i: number check last digit
+ * Return: 1
+ */
+
+int print_last_digit(int i)
+{
+	int c;
+
+	c = i % 10;
+	return (c);
+}

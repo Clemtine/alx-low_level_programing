6b95f162b9231254dc2151f078ec2bcae5222b9e
Clémentine AMEVOR <clementineamevor97@gmail.com>
2022-12-13 17:03:41 -0800

    task 7

---
+++ 7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - continuous learning
+ * @i: integer number
+ * Return: 1
+ */
+
+int print_last_digit(int i)
+{
+	int n;
+
+	n = i % 10;
+	return (n);
+}

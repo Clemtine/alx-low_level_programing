6f274f668dbca2ad549f78c501e2e0722e249015
Clémentine AMEVOR <clementineamevor97@gmail.com>
2022-12-14 16:14:21 -0800

    task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - learning
+ * Description: computer
+ * Return: 0
+ */
+
+int main(void)
+{
+	int s = 0;
+	int a = 0, b = 1, next = 0;
+
+	while (next < 4000000)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		if (next % 2 == 0)
+			s += next;
+	}
+	print("%i\n", sum);
+	return (0);
+}
+

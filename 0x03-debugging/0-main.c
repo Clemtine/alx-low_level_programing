64b2d1374f685150c969d745441d1863cbf67e39
Clémentine AMEVOR <clementineamevor97@gmail.com>
2022-12-15 13:30:09 -0800

    task0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - test function that prints if integer
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_ornegative(i);
+
+	return (0);
+}

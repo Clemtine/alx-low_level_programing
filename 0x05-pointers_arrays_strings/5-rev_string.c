ed705ec711154375ef09fd8df0bd4f070a0fab80
Clémentine AMEVOR <clementineamevor97@gmail.com>
2022-12-20 14:34:24 -0800

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - my function
+ * @s: string
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	int len = strlen(s);
+	int i;
+	char temp;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}

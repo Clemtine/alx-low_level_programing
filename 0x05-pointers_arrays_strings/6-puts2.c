a94490bba8e4d5f7a924004d01863baee3bccf0f
Clémentine AMEVOR <clementineamevor97@gmail.com>
2022-12-20 14:48:30 -0800

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - my learning function
+ * @str: string
+ *
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	if (dtr == NULL)
+	{
+		return;
+	}
+
+	for (int i = 0; str[i] != '\0'; i += 2)
+	{
+		printf("%c\n", str[i]);
+	}
+}

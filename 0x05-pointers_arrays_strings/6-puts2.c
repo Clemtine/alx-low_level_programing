00e61034520cd558a9eaef5e0311e703af8b7eb7
Clémentine AMEVOR <clementineamevor97@gmail.com>
2022-12-20 15:19:02 -0800

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,14 +10,16 @@
 
 void puts2(char *str)
 {
-	if (str == NULL)
-	{
-		return;
-	}
+	int i;
 
-	for (int i = 0; str[i] != '\0'; i += 2)
+	i = 0;
+	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
+		if ( i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
 	}
 	_putchar('\n');
 }

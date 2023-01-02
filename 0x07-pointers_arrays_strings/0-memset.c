bf7325a2b60bd31eb1ac02306e9ef2484f4bc75d
Clémentine AMEVOR <clementineamevor97@gmail.com>
2023-01-02 11:58:52 -0800

    0x07

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - a function
+ * @s: memory area
+ * @b: constant byte
+ * @n:  bytes to be filled
+ *
+ * Return: pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *point = s;
+
+	while (n--)
+	{
+		*s = b;
+		s++;
+	}
+	return (point);
+}
